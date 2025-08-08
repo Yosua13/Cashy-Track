1251a01286dc57be9214b522d260b5dea293578f
Yosua Reynaldi Manurun <reyyosua29@gmail.com>
2025-08-09 00:59:06 +0800

    Chore(api): menambahkan api untuk real device

---
+++ mobile/constants/api.ts
@@ -0,0 +1,8 @@
+// For android real device
+export const API_URL = "http://192.168.0.9:5001/api";
+
+// for emulator android
+// const API_URL = "http://10.0.2.2:5001/api";
+
+// for iOS and local
+// const API_URL = "http://localhost:5001/api";
